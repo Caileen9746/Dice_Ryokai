b607c9039257aa5fa0264bddda386bab92385fad
Caileen9746 <caileenblanc@gmail.com>
2026-09-02 13:44:50 +0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const DiceBox = window["dice-box-threejs"];
 
 const diceBox = new DiceBox("#dice-container", {
   assetPath: "./assets/",   // dossier PARENT de sounds/ et textures/
-  theme_texture: "Stained Glass",
+  theme_texture: "stainedglass",
   theme_material: "glass",   // astral fonctionne mieux avec "none" (voir texturelist.js)
   theme_colorset: "rainbow",
   sounds: true,
