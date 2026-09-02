7007dd8230a675490d4e63ca4ef6314b4043a889
Caileen9746 <caileenblanc@gmail.com>
2026-09-02 13:20:30 +0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const DiceBox = window["dice-box-threejs"];
 
 const diceBox = new DiceBox("#dice-container", {
   assetPath: "./assets/",   // dossier PARENT de sounds/ et textures/
-  theme_texture: "fire",
+  theme_texture: "astral",
   theme_material: "none",   // astral fonctionne mieux avec "none" (voir texturelist.js)
   theme_colorset: "white",
   sounds: true,
