8b4025ee77ffb2b0b8ccc7364b1a887812bc4196
Caileen9746 <caileenblanc@gmail.com>
2026-09-02 14:11:28 +0400

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const diceBox = new DiceBox("#dice-container", {
   assetPath: "./assets/",   // dossier PARENT de sounds/ et textures/
   theme_texture: "stainedglass",
   theme_material: "metal",   // astral fonctionne mieux avec "none" (voir texturelist.js)
-  theme_colorset: "starynight",
+  theme_colorset: "rainbow",
   sounds: true,
   volume: 80,
   sound_dieMaterial: "plastic", // astral fonctionne mieux avec "none" (voir texturelist.js)
