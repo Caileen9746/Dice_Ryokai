69263242e5f04f998bab8dfc01fe1231a398ebae
Caileen9746 <caileenblanc@gmail.com>
2026-09-02 15:12:58 +0400

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ const DiceBox = window["dice-box-threejs"];
 
 const diceBox = new DiceBox("#dice-container", {
   assetPath: "./assets/",
-  theme_material: "metal",
+  theme_material: "none",
   theme_customColorset: {
     background: "#9DE1FC",
     foreground: "#0D2157",
     outline: "black",
-    texture: "astral"
+    texture: "glitter"
   },
   sounds: false,
   shadows: true,
