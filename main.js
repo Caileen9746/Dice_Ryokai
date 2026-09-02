bfcebdb431add3e5edff1927e2710009ccb89de4
Caileen9746 <caileenblanc@gmail.com>
2026-09-02 13:37:50 +0400

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const diceBox = new DiceBox("#dice-container", {
   assetPath: "./assets/",   // dossier PARENT de sounds/ et textures/
   theme_texture: "glitter",
   theme_material: "wood",   // astral fonctionne mieux avec "none" (voir texturelist.js)
-  theme_colorset: "white",
+  theme_colorset: "water",
   sounds: true,
   volume: 80
 });
