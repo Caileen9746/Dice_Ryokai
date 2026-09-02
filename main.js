c15dc5ade7204e07e9af08e24c60d297b77b4a45
Caileen9746 <caileenblanc@gmail.com>
2026-09-02 13:19:57 +0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const DiceBox = window["dice-box-threejs"];
+
+const diceBox = new DiceBox("#dice-container", {
+  assetPath: "./assets/",   // dossier PARENT de sounds/ et textures/
+  theme_texture: "fire",
+  theme_material: "none",   // astral fonctionne mieux avec "none" (voir texturelist.js)
+  theme_colorset: "white",
+  sounds: true,
+  volume: 80
+});
+
+diceBox.initialize().then(() => {
+  console.log("🎲 Initialisé !");
+  diceBox.roll("1d20");
+});
